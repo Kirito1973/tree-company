3ffe44389830a1be151aee8ae5db87d77a1cfc48
Kirito1973 <kirigayakirito.95rh@gmail.com>
2026-07-25 23:49:41 +0400

    Update finance module initialization log message

--- tree-company/tree-admin/js/finance.js
+++ tree-company/tree-admin/js/finance.js
@@ -1,3 +1,3 @@
 // Этот файл зарезервирован для логики раздела ФИНАНСЫ.
 // Здесь будут графики, выплаты мастерам и подсчет выручки.
-console.log("Finance module initialized");
+console.log("Finance module initialized v5.6.0");
